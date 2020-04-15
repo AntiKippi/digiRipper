09278c16ca00c9e7c63e26087c6c78598363821d
AntiKippi <24373791+AntiKippi@users.noreply.github.com>
2020-04-15 17:31:12 +0200

    Added digiRipper script

---
+++ digiRipper.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+######################################################
+# digiRipper
+# Script to download books from digi4school.at
+#
+# Author: Kippi
+######################################################
+
+# --- BEGIN CONFIG ---
+
+# Book name used for the name of the pdf file
+BOOK_NAME="Test"
+
+# book id, as in the url (after '/ebook/')
+# (note that this is usually just one number, for example 2453), but it can also be muliple, like 2366/1)
+BOOK_ID=819
+
+# Number of pages
+PAGES=10
+
+# paste your cookie (from your logged in browser page) between the two single quotes below
+COOKIE='digi4b="1634726513%2c819%2c0%2c0%20{558%201586971556%20CAAA0AEA37850F9C195F5F9D291221FF7C454332}"; ad_session_id="1634560363%2c0%2c0%2c1586964429%20{375%201586965629%201AA16406BE9682001EE7E13CE6F2BCA00BC40A2F}"'
+
+# Directory to which files should be downloaded
+OUTPUT_DIRECTORY="/tmp/digi4school/$BOOK_ID"
+
+# Define if the downloaded files should be cleaned up after the pdf was created
+# 0 means "No", 1 means "Yes"
+CLEANUP=1
+
+# --- END CONFIG ---
+
+# DO NOT EDIT BELOW THIS LINE IF YOU DO NOT KNOW WHAT YOU ARE DOING
+
+if [ -z "$PAGES" ] || [ -z "$BOOK_ID" ] || [ -z "$COOKIE" ] || [ -z "$OUTPUT_DIRECTORY" ]; then
+    echo "ERROR: One or more configuration parameters are missing!"
+    exit 1
+fi
+
+PROTOCOL='https://'
+HOST='a.digi4school.at'
+BASE_FOLDER="$HOST/ebook/$BOOK_ID"
+BASE_URL="$PROTOCOL$BASE_FOLDER"
+
+interactive=1
+svg_files=""
+
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+while getopts "qd" opt; do
+    case "$opt" in
+    q)
+        interactive=0
+        ;;
+    d)
+        CLEANUP=0
+    esac
+done
+
+shift $((OPTIND-1))
+[ "${1:-}" = "--" ] && shift
+
+
+echo book $BOOK_ID, $PAGES pages
+echo
+
+# DOWNLOAD PAGES
+
+if (($interactive > 0)); then
+    echo 'Press return to start the download.'
+    read
+fi;
+
+mkdir -p "$OUTPUT_DIRECTORY"
+cd "$OUTPUT_DIRECTORY"
+
+echo 'Downloading svg pages...'
+
+for i in $(seq 1 $PAGES); do
+  wget --recursive --header="Cookie: $COOKIE" --header="Referer: $PROTOCOL$HOST/" --directory-prefix="$OUTPUT_DIRECTORY" --user-agent="Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0" -q "$BASE_URL/$i/$i.svg"
+  svg_files="$svg_files $OUTPUT_DIRECTORY/$BASE_FOLDER/$i/$i.svg"
+  percent=$(( (i * 100) / PAGES ))
+  echo -ne "\rProgress: $percent%"
+done
+
+echo
+echo 'Done downloading the pages.'
+
+
+# DOWNLOAD IMAGES
+
+echo
+echo 'Downloading embedded images...'
+
+for i in $(seq 1 $PAGES); do
+  cat "$OUTPUT_DIRECTORY/$BASE_FOLDER/$i/$i.svg" |
+  grep -oP 'xlink:href="\K(.*?)\.png(?=")' |
+  for f in $(</dev/stdin); do
+    wget --recursive --header="Cookie: $COOKIE" --header="Referer: $PROTOCOL$HOST/" --directory-prefix="$OUTPUT_DIRECTORY" --user-agent="Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0" -q "$BASE_URL/$i/$f";
+  done
+  percent=$(( (i * 100) / PAGES ))
+  echo -ne "\rProgress: $percent%"
+done
+
+echo
+echo 'Done downloading the images.'
+
+# Convert svg files to pdf
+
+echo
+echo 'Creating pdf...'
+
+rsvg-convert -f pdf -o "$OUTPUT_DIRECTORY/$BOOK_NAME.pdf" $svg_files
+
+if (($CLEANUP > 0)); then
+    echo 'Cleaning up...'
+    rm -rf "$OUTPUT_DIRECTORY/$HOST"
+fi;
+
+echo "Done! The pdf has beed saved to \"$OUTPUT_DIRECTORY/$BOOK_NAME.pdf\""
